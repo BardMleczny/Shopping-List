a4d6bbc787d91d18bb9ae63b48df7dad683898cc
BardMleczny <dominopl2016@wp.pl>
2024-12-18 19:40:15 +0100

    more responsivness

--- ShoppingList/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/ShoppingList.AssemblyInfo.cs
+++ ShoppingList/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/ShoppingList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+73bdba7b767a5aa09295c9710d035c6915f15a17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+83629b0934e67191c470362e10788e3371f6d7d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
